0f4ca0e0084b59a7a5f9c3d79194978e5b54f7cb
Diego Fernando Marquez Betancur <DFMarquezB@IQNetwork.hdq>
2018-12-18 12:28:18 -0500

    Adiciona proyecto inicial MilenioCloud

---
+++ MilenioCloudModel/MilenioCloudModel/Controllers/RolsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MilenioCloudModel.Models;
+
+namespace MilenioCloudModel.Controllers
+{
+    public class RolsController : Controller
+    {
+        private MilenioCloudEntities1 db = new MilenioCloudEntities1();
+
+        // GET: Rols
+        public ActionResult Index()
+        {
+            return View(db.Rols.ToList());
+        }
+
+        // GET: Rols/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rol rol = db.Rols.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // GET: Rols/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rols/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Codigo_Id,Nombre,Estado,Descripcion")] Rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                rol.Codigo_Id = Guid.NewGuid();
+                db.Rols.Add(rol);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rol);
+        }
+
+        // GET: Rols/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rol rol = db.Rols.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: Rols/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Codigo_Id,Nombre,Estado,Descripcion")] Rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rol).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rol);
+        }
+
+        // GET: Rols/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rol rol = db.Rols.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        // POST: Rols/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Rol rol = db.Rols.Find(id);
+            db.Rols.Remove(rol);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
