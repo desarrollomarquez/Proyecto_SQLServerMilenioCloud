56e1e2b3fa6721d15441f6dd9bece9c7cbf90d50
Diego Maŕquez <desarrollo.marquez@gmail.com>
2018-12-25 21:12:04 -0500

    Adiciona pruebas para insercion

--- 0.3 Importacion_MC_Seeders.sql
+++ 0.3 Importacion_MC_Seeders.sql
@@ -1,3 +1,5 @@
+--EXECUTE master.sys.sp_MSforeachdb 'USE [?]; SELECT i.table_catalog, * FROM information_schema.tables i, sys.tables t where t.name = i.TABLE_NAME and t.TYPE="U" and t.name like "%Cuota%"' 
+
 USE [MilenioCloud];
 GO
 BULK INSERT [dbo].[Poblado] FROM 'C:\Users\DFmarquezB\Documents\Proyecto_.Net\Modelo_MilenioCloud\poblados.csv'
@@ -19,3 +21,296 @@ FIELDTERMINATOR = ';',
 ROWTERMINATOR = '\n'
 );
 GO
+
+
+
+
+
+
+
+
+
+
+SELECT * FROM Entidad -- 800123654
+SELECT * FROM Usuario -- johan@unal.edu.co
+SELECT * FROM Entidad_Usuario
+
+
+
+
+
+DECLARE @TABLA_INTER NVARCHAR(100) = 'Entidad_Usuario';
+DECLARE @PARAMETROS NVARCHAR(500)= '800123654, johan@unal.edu.co, 2019-04-25T15:50:59.997,1, Nuevo Usuario en la compañia';
+DECLARE @CAMPOS		NVARCHAR(500);
+DECLARE @DATOS		NVARCHAR(500);
+DECLARE @QUERY		NVARCHAR(500);
+DECLARE @CANTIDAD_PARAMETROS	INT;
+DECLARE @CANTIDAD_CAMPOS     	INT;
+DECLARE @ret_code			    INT=0;
+
+
+BEGIN
+
+SELECT @CAMPOS = COALESCE(@CAMPOS + ', ', '') + COLUMN_NAME FROM  INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @TABLA_INTER
+
+SET @CANTIDAD_PARAMETROS =  (SELECT COUNT(VALUE) FROM STRING_SPLIT(@PARAMETROS, ','))
+SET @CANTIDAD_CAMPOS =  (SELECT COUNT(VALUE) FROM STRING_SPLIT(@CAMPOS, ','))
+
+IF @CAMPOS IS NULL
+	BEGIN
+	RAISERROR ('La tabla no existe', 16, 1);
+	--RETURN (1)
+	END
+
+ELSE 
+	IF @CANTIDAD_PARAMETROS = @CANTIDAD_CAMPOS
+		BEGIN
+		SET @DATOS = ( SELECT STUFF(
+					(	SELECT
+						CAST(', ' AS VARCHAR(MAX)) + 
+						CASE 
+						WHEN ISNUMERIC(value) = 1 THEN  value
+						WHEN ISDATE(value) = 1 THEN 'CAST('+''''+value+''''+' AS DATETIME )' --'CAST('+''''+substring(TRIM(value),0,12)+''''+' AS DATETIME )'
+						ELSE ''''+value+''''
+						END
+						FROM STRING_SPLIT(@PARAMETROS, ',')
+						FOR XML PATH('')
+					), 1, 1, '') as DATOS
+				 )
+		
+		SET @QUERY = N'INSERT INTO '+@TABLA_INTER+' ( '+@CAMPOS +' ) VALUES ( '+@DATOS+' )';
+		--SET @QUERY = N' SELECT '+@CAMPOS+' FROM '+@TABLA_INTER;
+		BEGIN TRY  
+			EXECUTE sp_executesql @QUERY;
+			--RETURN (0);
+		END TRY
+		BEGIN CATCH  
+		SELECT   
+			ERROR_NUMBER() AS ErrorNumber,   
+			ERROR_PROCEDURE() AS ErrorProcedure,  
+			ERROR_MESSAGE() AS ErrorMessage;
+		END CATCH;
+		END
+	ELSE
+		BEGIN
+		RAISERROR ('Los parametros de entrada no coinciden con los campos de la tabla a insertar ', 16, 1);
+		--RETURN (1); 
+		END
+
+END
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+SELECT * FROM Entidad
+
+select * from sys.messages
+where text like '%table exists%'
+
+
+
+--'Entidad', '800123654, LA CAYENA, 110, 255, 2018-04-25T15:50:59.997,2018-04-26T15:50:59.997'; 
+--'Usuario','johan@unal.edu.co, 123, 123, 1';
+--'Rol', 'Director Academico, 1, Encargado de Docentes';
+
+--'Departamento', '16, PUERTO OSCURO';
+--'Municipio', '16, 22222, ESCONDITE';
+--'Poblado', '22222, 44,  A PESO, MC';
+--'Telefono', '16, PUERTO OSCURO';
+
+
+DECLARE @ret_code INT;
+EXECUTE @ret_code = sp_mc_insert 'Entidad', '800123654, LA CAYENA, 110, 255, 2018-04-25T15:50:59.997,2018-04-26T15:50:59.997';
+SELECT @ret_code;
+
+
+SELECT * FROM Entidad
+SELECT * FROM Usuario
+SELECT * FROM Rol
+SELECT * FROM Telefono
+SELECT * FROM Departamento
+SELECT * FROM Municipio WHERE Dane_Id=22222
+SELECT * FROM Poblado
+
+
+
+DELETE FROM Entidad
+WHERE Codigo_Id IN ('EA5F5C66-6E47-41D8-BE43-10761FDA599E',
+'4D30A61D-3531-4750-AC7A-279141603053',
+'608A2862-F3E8-4188-A191-43FE130928D4',
+'EFE496C6-CBB1-44CC-9766-CFCD95905D2E',
+'954FB448-3F98-4EBA-AAAE-E0453EA031B7');
+
+
+
+    
+ CREATE TABLE dbo.Entidad(
+  Codigo_Id		 UNIQUEIDENTIFIER NOT NULL ,
+  Nit			 INT  NOT NULL,
+  Nombre		 VARCHAR(200) NOT NULL,
+  CodigoEntidad  INT NOT NULL,
+  CodigoDane  	 INT NOT NULL,
+  Telefono       VARCHAR(20) NOT NULL,
+  FiniFiscal     DATETIME NOT NULL,
+  FfinFiscal     DATETIME NOT NULL
+  CONSTRAINT PK_Entidad primary key (Nit)
+ );
+
+  CREATE TABLE dbo.Telefono(
+  Codigo_Id		 UNIQUEIDENTIFIER NOT NULL ,
+  Sucursal_Id    UNIQUEIDENTIFIER NOT NULL ,
+  Numero		 INT NOT NULL,
+  Tipo			 VARCHAR(20) NOT NULL
+  CONSTRAINT PK_Telefono primary key (Numero)
+ );
+
+ CREATE TABLE dbo.Sucursal (
+  Codigo_Id	      UNIQUEIDENTIFIER NOT NULL,
+  Entidad_Id      INT  NOT NULL,
+  Nombre          VARCHAR(200) NOT NULL,
+  Ubicacion_Id    UNIQUEIDENTIFIER NOT NULL,
+  Principal       BIT, 
+  CONSTRAINT PK_Sucursal primary key (Codigo_Id),
+  CONSTRAINT FK_SEntidad FOREIGN KEY (Entidad_Id) REFERENCES Entidad (Nit) ON DELETE CASCADE
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--0	Successful execution.
+--1	Required parameter value is not specified.
+--2	Specified parameter value is not valid.
+--3	Error has occurred getting sales value.
+--4	NULL sales value found for the salesperson.
+
+
+
+
+
+
+
+
+BEGIN
+
+DECLARE @Iteration Integer = 0;
+DECLARE @CAMPO   VARCHAR(300);
+DECLARE @CANTIDAD VARCHAR(100);
+
+SET @CANTIDAD = (SELECT COUNT(*)  FROM  INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Municipio' )
+
+	WHILE @Iteration < @CANTIDAD  
+	BEGIN  
+		SET @CAMPO = (             SELECT 
+									CASE
+									WHEN COLUMN_NAME = 'Codigo_Id' THEN ''
+									ELSE COLUMN_NAME
+									END COLUMN_NAME
+									FROM  INFORMATION_SCHEMA.COLUMNS
+									WHERE TABLE_NAME = 'Municipio'
+									FOR XML PATH ('')
+									 )
+	SET @Iteration += 1  
+	END;
+SELECT @CAMPO;
+END 
+
+
+SELECT 
+@DATOS = COALESCE( 
+(SELECT
+CASE 
+WHEN ISNUMERIC(value) = 1 THEN value
+ELSE 'CAST( '+value+' AS VARCHAR(150))'
+END 
+FROM STRING_SPLIT(@PARAMETROS, ',')) + ', ', '' )  ;	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--0	Successful execution.
+--1	Required parameter value is not specified.
+--2	Specified parameter value is not valid.
+--3	Error has occurred getting sales value.
+--4	NULL sales value found for the salesperson.
+
+
+
+
+
+
+
+
+BEGIN
+
+DECLARE @Iteration Integer = 0;
+DECLARE @CAMPO   VARCHAR(300);
+DECLARE @CANTIDAD VARCHAR(100);
+
+SET @CANTIDAD = (SELECT COUNT(*)  FROM  INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'Municipio' )
+
+	WHILE @Iteration < @CANTIDAD  
+	BEGIN  
+		SET @CAMPO = (             SELECT 
+									CASE
+									WHEN COLUMN_NAME = 'Codigo_Id' THEN ''
+									ELSE COLUMN_NAME
+									END COLUMN_NAME
+									FROM  INFORMATION_SCHEMA.COLUMNS
+									WHERE TABLE_NAME = 'Municipio'
+									FOR XML PATH ('')
+									 )
+	SET @Iteration += 1  
+	END;
+SELECT @CAMPO;
+END 
+
+
+SELECT 
+@DATOS = COALESCE( 
+(SELECT
+CASE 
+WHEN ISNUMERIC(value) = 1 THEN value
+ELSE 'CAST( '+value+' AS VARCHAR(150))'
+END 
+FROM STRING_SPLIT(@PARAMETROS, ',')) + ', ', '' )  ;	
